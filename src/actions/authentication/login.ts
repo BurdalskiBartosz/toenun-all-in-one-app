a7500ed0b890e9fef34ea5a32effb5058cbc8db1
Bartosz <bartosz.burdalski@gmail.com>
2024-10-25 20:34:39 +0200

    Update action

--- src/actions/authentication/login.ts
+++ src/actions/authentication/login.ts
@@ -2,11 +2,17 @@
 
 import { signIn, signOut } from "@/auth";
 import type { LoginFormType } from "@/types/forms";
+import { loginSchema } from "@/utils/validations";
 import { AuthError } from "next-auth";
 
-export async function login(data: LoginFormType) {
+export async function login(values: LoginFormType) {
+  const parsedValues = loginSchema.safeParse(values);
+
+  if (!parsedValues.success) {
+    return { error: "Invalid data!" };
+  }
   try {
-    await signIn("credentials", data);
+    await signIn("credentials", values);
   } catch (error) {
     if (error instanceof AuthError) {
       throw error;
